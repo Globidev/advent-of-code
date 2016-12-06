2d96cd44e6dca2a31a31d3334fa44f1d17c3705f
Guillaume Depardon <guillaume.depardon@gmail.com>
2016-12-06 02:21:40 +0100

    [4   | meta-cpp] :lipstick:

--- 2016/day-4/puzzle-0/main.cpp
+++ 2016/day-4/puzzle-0/main.cpp
@@ -1,235 +1,86 @@
-#define let const auto
-#define let_ constexpr auto
-
-#define BOOST_HANA_CONFIG_ENABLE_STRING_UDL
-#include <boost/hana.hpp>
-using namespace boost::hana;
-using namespace boost::hana::literals;
-
-template <std::size_t count>
-struct Variadic {
-
-    struct Constructor {
-        template <class... Ts>
-        let_ operator()(Ts...) const {
-            static_assert(sizeof...(Ts) == count);
-            return tuple<Ts...> { };
-        }
-    };
-
-    static let_ constructor = Constructor { };
-
-    template <std::size_t i>
-    struct Getter {
-        template <class... Ts>
-        let_ operator()(const tuple<Ts...> t) const { return at_c<i>(t); }
-    };
-
-    template <std::size_t i>
-    static let_ getter = Getter<i> { };
-
-};
+#include "boilerplate.hpp"
 
 struct Room: Variadic<3> {
-    static let_ name = getter<0>;
-    static let_ sector_id = getter<1>;
-    static let_ given_checksum = getter<2>;
+    static let name = getter<0>;
+    static let sector_id = getter<1>;
+    static let given_checksum = getter<2>;
 };
 
-constexpr decltype(Room::name) Room::name;
-constexpr decltype(Room::sector_id) Room::sector_id;
-constexpr decltype(Room::given_checksum) Room::given_checksum;
+let unpack_to_tuple = unpack & make_tuple;
 
-struct ToDigitConverter {
-    template <class C>
-    let_ operator()(C c) const { return int_c<c.value - '0'>; }
-};
-let_ to_digit = ToDigitConverter { };
+let checksum_size           = size_c<5>;
+let bracketed_checksum_size = checksum_size + size_c<2>;
 
-struct Multiplier {
-    template <class A, class B>
-    let_ operator()(A a, B b) const { return a * b; }
-};
-let_ multiply = Multiplier { };
+let extract_checksum =
+    (take_back & bracketed_checksum_size) >>
+    drop_back >> drop_front;
 
-let_ to_int = compose(
-    reverse_partial(
-        fold_left,
-        0_c,
-        lockstep(plus)(
-            partial(
-                multiply,
-                10_c
-            ),
-            id
-        )
-    ),
-    reverse_partial(
-        transform,
-        to_digit
-    )
-);
+let not_dash = not_equal.to(char_c<'-'>);
 
-// The checksum is between square brackets at the end of the description
-// => take the (size + 2) last characters and remove the surrounding brackets
-let_ checksum_size           = size_c<5>;
-let_ bracketed_checksum_size = checksum_size + size_c<2>;
+let extract_room_sector_id =
+    reverse >>
+    (drop_front & bracketed_checksum_size) >>
+    (take_while & not_dash) >>
+    reverse;
 
-let_ extract_checksum = compose(
-    // Remove both ends
-    compose(
-        drop_front,
-        drop_back
-    ),
-    // Keep the bracketed checksum
-    reverse_partial(
-        take_back,
-        bracketed_checksum_size
-    )
-);
+let extract_room_name =
+    reverse >>
+    (drop_while & not_dash) >>
+    drop_front >>
+    reverse;
 
-// From right to left, the sector id is immediately after the bracketed checksum
-// up until the first encountered dash character
-let_ not_dash = not_equal.to(char_c<'-'>);
+let to_digit = minus & char_c<'0'>;
+let to_int =
+    (transform & to_digit) >>
+    (fold_left & 0_c & lockstep(plus)(mult | 10_c, id));
 
-let_ extract_room_sector_id = compose(
-    // Revert back
-    reverse,
-    // Keep until first dash
-    reverse_partial(
-        take_while,
-        not_dash
-    ),
-    // Skip the bracketed checksum
-    reverse_partial(
-        drop_front,
-        bracketed_checksum_size
-    ),
-    // From right to left
-    reverse
-);
+let parse_room =
+    unpack_to_tuple >>
+    make_tuple >>
+    (ap | make_tuple(
+        extract_room_name,
+        extract_room_sector_id >> to_int,
+        extract_checksum
+    )) >>
+    (unpack & Room::constructor);
 
-let_ extract_room_name = compose(
-    reverse,
-    drop_front,
-    reverse_partial(drop_while, not_dash),
-    reverse
-);
+// The checksum is the 5 most common letters present in the ciphered name
+let compute_checksum =
+    Room::name >>
+    // Remove the dashes
+    (filter & not_dash) >>
+    // Sort letters (to break ties)
+    sort >>
+    // Group letters by packs
+    group >>
+    // Sort the groups by length
+    (sort & (greater ^on^ size)) >>
+    // Return the common letter in the 5 first groups
+    (transform & (at & 0_c)) >>
+    (take_front & checksum_size);
 
-// struct RoomParser {
+let is_room_valid = demux(equal)(compute_checksum, Room::given_checksum);
 
-//     template <class String>
-//     let_ operator()(String raw_room_description) const {
-//         let_ room_description = unpack(raw_room_description, make<tuple_tag>);
+let day_4_0_impl =
+    (transform & parse_room) >>
+    (filter & is_room_valid) >>
+    (fold_left & 0_c & lockstep(plus)(id, Room::sector_id));
 
-//         let_ room_checksum = extract_checksum(room_description);
-//         let_ room_sector_id = extract_room_sector_id(room_description);
-//         // The name starts at the beginning and is what's left when you remove the
-//         // bracketed checksum and the sector with its separating dash
-//         let_ size_to_drop = (
-//             bracketed_checksum_size +        // Bracketed checksum size
-//             size(room_sector_id) + size_c<1> // Extra separating dash
-//         );
-//         let_ room_name = drop_back(room_description, size_to_drop);
-
-//         return Room::constructor(room_name, to_int(room_sector_id), room_checksum);
-//     }
-
-// };
-
-let_ parse_room = compose(
-    reverse_partial(
-        unpack,
-        Room::constructor
-    ),
-    partial(
-        ap,
-        make_tuple(
-            extract_room_name,
-            compose(to_int, extract_room_sector_id),
-            extract_checksum
-        )
-    ),
-    make_tuple,
-    reverse_partial(
-        unpack,
-        make<tuple_tag>
-    )
-);
-
-// The checksum is the 5 most common letters present in the ciphered name:
-//   1) remove the dashes
-//   2) sort letters (to break ties)
-//   3) group letters by packs
-//   4) sort the groups by length
-//   5) return the letters in the 5 first groups
-let_ compute_checksum = compose(
-    // Return the first 5 letters (5)
-    reverse_partial(
-        take_front,
-        checksum_size
-    ),
-    // Map each group to its corresponding letter (taking the first element)
-    reverse_partial(
-        transform,
-        reverse_partial(
-            at,
-            0_c
-        )
-    ),
-    // Sort groups by lengths descending (4)
-    reverse_partial(
-        sort,
-        greater ^on^ size
-    ),
-    // Group letters (3)
-    group,
-    // Sort letters (2)
-    sort,
-    // Remove the dashes (1)
-    reverse_partial(
-        filter,
-        not_dash
-    ),
-    // Use the room name
-    Room::name
-);
-
-let_ is_room_valid = demux(equal)(compute_checksum, Room::given_checksum);
-
-let_ day_4_0 = demux(
-    compose(
-        reverse_partial(
-            fold_left,
-            0_c,
-            lockstep(plus)(
-                id,
-                Room::sector_id
-            )
-        ),
-        reverse_partial(
-            filter,
-            is_room_valid
-        ),
-        reverse_partial(
-            transform,
-            parse_room
-        )
-    )
-)(make_tuple);
+let day_4_0 = variadicly(day_4_0_impl);
 
 int main() {
     // Example
-    let_ check_room = compose(is_room_valid, parse_room);
-    static_assert(check_room("aaaaa-bbb-z-y-x-123[abxyz]"_s) == true);
-    static_assert(check_room("a-b-c-d-e-f-g-h-987[abcde]"_s) == true);
-    static_assert(check_room("not-a-real-room-404[oarel]"_s) == true);
-    static_assert(check_room("totally-real-room-200[decoy]"_s) == false);
+    let check_room_description = parse_room >> is_room_valid;
+    static_assert(check_room_description("aaaaa-bbb-z-y-x-123[abxyz]"_s) == true);
+    static_assert(check_room_description("a-b-c-d-e-f-g-h-987[abcde]"_s) == true);
+    static_assert(check_room_description("not-a-real-room-404[oarel]"_s) == true);
+    static_assert(check_room_description("totally-real-room-200[decoy]"_s) == false);
 
     static_assert(day_4_0(
         "aaaaa-bbb-z-y-x-123[abxyz]"_s,
         "a-b-c-d-e-f-g-h-987[abcde]"_s,
         "not-a-real-room-404[oarel]"_s,
-        "totally-real-room-200[decoy]"_s
-    ) == 123 + 987 + 404);
+        "totally-real-room-200[decoy]"_s,
+        "vxupkizork-sgmtkzoi-pkrrehkgt-zxgototm-644[kotgr]"_s
+    ) == 123 + 987 + 404 + 644);
 }
--- 2016/day-4/puzzle-1/main.cpp
+++ 2016/day-4/puzzle-1/main.cpp
@@ -2,43 +2,40 @@
 
 // Data type
 struct Room: Variadic<3> {
-    static let_ name = getter<0>;
-    static let_ sector_id = getter<1>;
-    static let_ given_checksum = getter<2>;
+    static let name = getter<0>;
+    static let sector_id = getter<1>;
+    static let given_checksum = getter<2>;
 };
 
-let_ unpack_to_tuple = unpack & make_tuple;
+let unpack_to_tuple = unpack & make_tuple;
 
-let_ checksum_size           = size_c<5>;
-let_ bracketed_checksum_size = checksum_size + size_c<2>;
+let checksum_size           = size_c<5>;
+let bracketed_checksum_size = checksum_size + size_c<2>;
 
-let_ extract_checksum =
+let extract_checksum =
     (take_back & bracketed_checksum_size) >>
     drop_back >> drop_front;
 
-let_ not_dash = not_equal.to(char_c<'-'>);
+let not_dash = not_equal.to(char_c<'-'>);
 
-let_ extract_room_sector_id =
+let extract_room_sector_id =
     reverse >>
     (drop_front & bracketed_checksum_size) >>
     (take_while & not_dash) >>
     reverse;
 
-let_ extract_room_name =
+let extract_room_name =
     reverse >>
     (drop_while & not_dash) >>
     drop_front >>
     reverse;
 
-let_ to_digit = minus & char_c<'0'>;
-let_ to_int =
+let to_digit = minus & char_c<'0'>;
+let to_int =
     (transform & to_digit) >>
-    (reverse_partial(
-        fold_left, 0_c,
-        lockstep(plus)(mult | 10_c, id)
-    ));
+    (fold_left & 0_c & lockstep(plus)(mult | 10_c, id));
 
-let_ parse_room =
+let parse_room =
     unpack_to_tuple >>
     make_tuple >>
     (ap | make_tuple(
@@ -48,12 +45,12 @@ let_ parse_room =
     )) >>
     (unpack & Room::constructor);
 
-let_ a_ = char_c<'a'>;
+let a_ = char_c<'a'>;
 
 // This one is painfull to write in point free...
 struct NRotator {
     template <class X, class C>
-    let_ operator()(X x, C c) const {
+    let operator()(X x, C c) const {
         return if_(
             not_dash(c),
             (c - a_ + x) % 26_c + a_,
@@ -61,9 +58,9 @@ struct NRotator {
         );
     }
 };
-let_ rotn = NRotator{};
+let rotn = NRotator{};
 
-let_ deciphered_room_name =
+let deciphered_room_name =
     make_tuple >>
     (ap | make_tuple(
         Room::name,
@@ -71,18 +68,18 @@ let_ deciphered_room_name =
     )) >>
     (unpack & transform);
 
-let_ northpole_room_name = unpack_to_tuple("northpole object storage"_s);
+let northpole_room_name = unpack_to_tuple("northpole object storage"_s);
 
-let_ is_north_pole_object_storage_room =
+let is_north_pole_object_storage_room =
     deciphered_room_name >>
     equal.to(northpole_room_name);
 
-let_ day_4_1_impl =
+let day_4_1_impl =
     (transform & parse_room) >>
     (find_if   & is_north_pole_object_storage_room) >>
     (ap        | just(Room::sector_id));
 
-let_ day_4_1 = variadicly(day_4_1_impl);
+let day_4_1 = variadicly(day_4_1_impl);
 
 int main() {
     static_assert(day_4_1(
