7c389e2ab8df42185aa025f451d91ff7dbec762a
Guillaume Depardon <guillaume.depardon@gmail.com>
2018-02-04 18:28:13 +0100

    [2015 Day 04] Parallelize the algorithm

--- 2015/rust/src/day04.rs
+++ 2015/rust/src/day04.rs
@@ -2,6 +2,9 @@ extern crate crypto;
 
 use day04::crypto::md5::Md5;
 use day04::crypto::digest::Digest;
+use std::thread;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicU32, Ordering};
 
 fn md5(input: &str) -> String {
     let mut md5 = Md5::new();
@@ -9,16 +12,50 @@ fn md5(input: &str) -> String {
     md5.result_str()
 }
 
+fn md5_step_compute(key: String, start: u32, step: u32, pat: &str, spinlock: Arc<AtomicU32>) {
+    let mut iter = start;
+    loop {
+        let stored = spinlock.load(Ordering::SeqCst);
+        if stored == 0 || iter < stored {
+            let seed = format_args!("{}{}", key, iter).to_string();
+            if md5(&seed).starts_with(pat) {
+                spinlock.store(iter, Ordering::SeqCst);
+            }
+            iter += step;
+        }
+        else {
+            break
+        }
+    }
+}
+
+fn mine_block_threaded(th_count: u32, key: &str, pattern: &'static str) -> u32 {
+    let spinlock = Arc::new(AtomicU32::new(0));
+
+    let threads: Vec<_> = (0u32..th_count).map(|i| {
+        let key_clone = key.to_string().clone();
+        let spinlock_clone = spinlock.clone();
+        thread::spawn(move || {
+            md5_step_compute(key_clone, i, th_count, pattern, spinlock_clone)
+        })
+    }).collect();
+
+    for thread in threads {
+        match thread.join() {
+            Err(e) => panic!(e),
+            _ => ()
+        }
+    }
+
+    spinlock.load(Ordering::SeqCst)
+}
+
 pub fn p1(input: &str) -> u32 {
-    (0u32..).find(|i| {
-        let seed = format_args!("{}{}", input.trim(), i).to_string();
-        md5(&seed).starts_with("00000")
-    }).unwrap()
+    let key = input.trim();
+    mine_block_threaded(4, key, "00000")
 }
 
 pub fn p2(input: &str) -> u32 {
-    (0u32..).find(|i| {
-        let seed = format_args!("{}{}", input.trim(), i).to_string();
-        md5(&seed).starts_with("000000")
-    }).unwrap()
+    let key = input.trim();
+    mine_block_threaded(4, key, "000000")
 }
