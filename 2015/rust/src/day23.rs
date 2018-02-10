cbcabdc1b7578cd4ff74bfda4d98b2d376e0cfe3
Guillaume Depardon <guillaume.depardon@gmail.com>
2018-02-10 19:05:48 +0100

    :lipstick:

--- 2015/rust/src/day23.rs
+++ 2015/rust/src/day23.rs
@@ -38,34 +38,45 @@ fn parse_instruction(raw_instruction: &str) -> Instruction {
         (if sign == "+".as_bytes() { value } else { -value })
     ));
 
-    named!(hlf<Instruction>, do_parse!(tag_s!("hlf ") >> reg: register >> (Hlf(reg))));
-    named!(tpl<Instruction>, do_parse!(tag_s!("tpl ") >> reg: register >> (Tpl(reg))));
-    named!(inc<Instruction>, do_parse!(tag_s!("inc ") >> reg: register >> (Inc(reg))));
-    named!(jmp<Instruction>, do_parse!(tag_s!("jmp ") >> off: offset   >> (Jmp(off))));
+    named!(hlf<Instruction>, do_parse!(
+        tag_s!("hlf ") >> reg: register >> (Hlf(reg))
+    ));
+    named!(tpl<Instruction>, do_parse!(
+        tag_s!("tpl ") >> reg: register >> (Tpl(reg))
+    ));
+    named!(inc<Instruction>, do_parse!(
+        tag_s!("inc ") >> reg: register >> (Inc(reg))
+    ));
+    named!(jmp<Instruction>, do_parse!(
+        tag_s!("jmp ") >> off: offset   >> (Jmp(off))
+    ));
     named!(jie<Instruction>, do_parse!(
-        tag_s!("jie ") >>
-        reg: register  >>
-        tag_s!(", ")   >>
-        off: offset    >>
-        (Jie(reg, off))
+        tag_s!("jie ") >> reg: register >>
+        tag_s!(", ")   >> off: offset   >> (Jie(reg, off))
     ));
     named!(jio<Instruction>, do_parse!(
-        tag_s!("jio ") >>
-        reg: register  >>
-        tag_s!(", ")   >>
-        off: offset    >>
-        (Jio(reg, off))
+        tag_s!("jio ") >> reg: register >>
+        tag_s!(", ")   >> off: offset   >> (Jio(reg, off))
     ));
 
     named!(instruction<Instruction>, alt!(hlf | tpl | inc | jmp | jie | jio));
 
     match instruction(raw_instruction.as_bytes()) {
         IResult::Done(_, instruction) => instruction,
-        _                           => panic!("Wrong character format")
+        _                             => panic!("Wrong instruction format")
     }
 }
 
-fn run(instructions: &Vec<Instruction>, registers: &mut Registers) {
+fn update_reg(reg: &Register, regs: &mut Registers, f: fn(u32) -> u32) {
+    let entry = regs.entry(reg.clone()).or_insert(0);
+    *entry = f(*entry);
+}
+
+fn get_reg(reg: &Register, regs: &mut Registers) -> u32 {
+    regs.get(reg).map_or(0, Clone::clone)
+}
+
+fn run(instructions: &Vec<Instruction>, regs: &mut Registers) {
     use self::Instruction::*;
 
     let mut pc = 0i32;
@@ -74,36 +85,16 @@ fn run(instructions: &Vec<Instruction>, registers: &mut Registers) {
         match instructions.get(pc as usize) {
             None => break,
             Some(instr) => {
-                match instr {
-                    &Hlf(ref r)    => {
-                        let entry = registers.entry(r.clone()).or_insert(0);
-                        *entry = *entry / 2;
-                    },
-                    &Tpl(ref r)    => {
-                        let entry = registers.entry(r.clone()).or_insert(0);
-                        *entry = *entry * 3;
-                    },
-                    &Inc(ref r)    => {
-                        let entry = registers.entry(r.clone()).or_insert(0);
-                        *entry = *entry + 1;
-                    },
-                    &Jmp(ref o)    => {
-                        pc += o - 1;
-                    },
-                    &Jie(ref r, o) => {
-                        let entry = registers.entry(r.clone()).or_insert(0);
-                        if *entry % 2 == 0 {
-                            pc += o - 1;
-                        }
-                    },
-                    &Jio(ref r, o) => {
-                        let entry = registers.entry(r.clone()).or_insert(0);
-                        if *entry == 1 {
-                            pc += o - 1;
-                        }
-                    },
+                let mut pc_offset = 1;
+                match *instr {
+                    Hlf(ref r)    => update_reg(r, regs, |v| v / 2),
+                    Tpl(ref r)    => update_reg(r, regs, |v| v * 3),
+                    Inc(ref r)    => update_reg(r, regs, |v| v + 1),
+                    Jmp(o)        => pc_offset = o,
+                    Jie(ref r, o) => if get_reg(r, regs) % 2 == 0 { pc_offset = o; },
+                    Jio(ref r, o) => if get_reg(r, regs) == 1     { pc_offset = o; },
                 }
-                pc += 1;
+                pc += pc_offset;
             }
         }
     }
