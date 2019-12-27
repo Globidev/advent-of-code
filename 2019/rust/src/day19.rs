83fb27c2847cb59bf1287d72a7d2b96236e11968
Guillaume Depardon <guillaume.depardon@gmail.com>
2019-12-27 03:20:36 +0100

    Day 19

---
+++ 2019/rust/src/day19.rs
@@ -0,0 +1,89 @@
+use std::fmt::Debug;
+use itertools::Itertools;
+use crate::intcode::{Int, vm::VirtualMachine, io::ext::{Split, Iter, SingleOutput}};
+use std::array::IntoIter;
+use rayon::prelude::*;
+
+const RAW_INPUT_STR: &str = include_str!("../../inputs/day19.txt");
+
+pub fn day19() -> impl Debug {
+    let program = parse_input(RAW_INPUT_STR).collect_vec();
+
+    (part1(&program), part2(&program))
+}
+
+pub fn part1(program: &[Int]) -> usize {
+    beam_map(program, 50, 50)
+        .filter(|&state| state == DroneState::PulledIn)
+        .count()
+}
+
+pub fn part2(program: &[Int]) -> usize {
+    let w = 1200_usize;
+    let h = 1200_usize;
+
+    let coordinates = (100..h)
+        .into_par_iter()
+        .flat_map(move |y| (100..w).into_par_iter().map(move |x| (x, y)));
+
+    let (x, y) = coordinates
+        .find_first(|&(x, y)| {
+            let corners = [(x, y), (x + 99, y), (x, y + 99), (x + 99, y + 99)];
+            IntoIter::new(corners)
+                .all(|(x, y)| beam_state(program, x, y) == DroneState::PulledIn)
+        })
+        .expect("No place to put the ship");
+
+    x * 10_000 + y
+}
+
+fn beam_state(program: &[Int], x: usize, y: usize) -> DroneState {
+    let mut output = SingleOutput::new();
+    let input = Iter(IntoIter::new([x as _, y as _]));
+
+    let vm = VirtualMachine::new(program, Split(input, &mut output));
+    vm.run();
+
+    match output.get().expect("No output ?") {
+        0 => DroneState::Stationary,
+        1 => DroneState::PulledIn,
+        invalid => panic!("Invalid state output: {}", invalid),
+    }
+}
+
+fn beam_map(program: &[Int], width: usize, height: usize) -> impl ParallelIterator<Item = DroneState> + '_ {
+    let coordinates = (0..height)
+        .into_par_iter()
+        .flat_map(move |y| (0..width).into_par_iter().map(move |x| (x, y)));
+
+    coordinates
+        .map(move |(x, y)| beam_state(program, x, y))
+}
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum DroneState {
+    Stationary,
+    PulledIn,
+}
+
+pub fn parse_input(input: &str) -> impl Iterator<Item = Int> + '_ {
+    input.split(',')
+        .map(|raw_number| raw_number.parse().expect("Invalid integer code"))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn p1() {
+        let code = parse_input(RAW_INPUT_STR).collect_vec();
+
+        assert_eq!(part1(&code), 152);
+    }
+
+    #[test]
+    fn p2() {
+        let code = parse_input(RAW_INPUT_STR).collect_vec();
+
+        assert_eq!(part2(&code), 10_730_411);
+    }
+}
